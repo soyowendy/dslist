4a1292ba6c7233b1e39cfe8e793f3ea826ecca5d
Wenderson Santos <souowendy@gmail.com>
2023-07-28 14:08:15 -0300

    Create Profiles and Updated searchByList query for Postgresql

--- src/main/java/com/wendy/dslist/projection/GameMinProjection.java
+++ src/main/java/com/wendy/dslist/projection/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.wendy.dslist.projection;
 public interface GameMinProjection {
 	Long getId();
 	String getTitle();
-	Integer getYear();
+	Integer getGameYear();
 	String getImgUrl();
 	String getShortDescription();
 	Integer getPosition();
